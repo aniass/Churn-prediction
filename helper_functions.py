f769401411e4f79282f23e44ea068d660c512dab
Aniass <a.staskiewicz4@gmail.com>
2024-09-15 15:24:01 +0200

    added evaluation function

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, roc_curve
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
 
 
 def plot_roc_curve(model, X_test, y_test):
@@ -25,3 +27,19 @@ def plot_conf_matrix(pred_set, y_test):
 
     plt.title("Confusion Matrix",fontsize=14)
     plt.show()
+    
+
+def evaluation(model, X_train, y_train, X_test, y_test):
+    '''Accuracy score and ROC AUC score calculation; ROC curve and confusion matrix plots'''
+    # Accuracy score
+    acc = cross_val_score(model, X_train, y_train, cv=10, scoring='accuracy')
+    acc_score = round(acc.mean(), 2)
+    print('Accuracy score: %s' % acc_score)
+    # ROC AUC score
+    pred_prob = model.predict_proba(X_test)
+    score = roc_auc_score(y_test, pred_prob[:,1])
+    roc_score = round(score, 2)
+    print('ROC AUC score: %s' % roc_score)
+    pred_y = model.predict(X_test)
+    return pred_y 
+    
